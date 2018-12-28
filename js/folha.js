7d5dc8e7735d7b70bcce3e6b89559bf054bc8476
alanliveira <alanliveira@outlook.com>
2018-12-28 18:40:41 -0200

    refatoração dos arquivos para o padrão MVC

--- js/folha.js
+++ js/folha.js
@@ -1,64 +1,11 @@
-function calcularFolha() {
-    let empregado = obterEmpregado()
+function calcularFolha(empregado) {
     let provento = calcularProventos(empregado)
     let desconto = calcularDescontos(provento)
+    let salarioLiquido = obterTotal(provento, desconto)
 
-    let salarioLiquido = provento.total - desconto.total
-
-    mostrarFolha({empregado, provento, desconto, salarioLiquido})
+    return {empregado, provento, desconto, salarioLiquido};
 }
 
-function obterEmpregado () {
-    return {
-        empregado : getInputSeletor('txtEmpregado'),
-        salario : parseFloat(getInputSeletor('txtSalario'))
-    }
-}
-
-function getInputSeletor(value) {
-    return document.getElementById(value).value
-}
-
-//Função que irá gerar uma table para ser mostrada na página html
-function mostrarFolha(object) {
-    console.log(object)
-    document.getElementById('result-folha').innerHTML = `
-    <h3>Folha de pagamento do empregado ${object.empregado.empregado} </h3>
-    <table>
-        <tr>
-            <th>Descrição</th>
-            <th>Provento</th>
-            <th>Desconto</th>
-        </tr>
-        <tr>
-            <td>Salário</td>
-            <td>${(object.provento.salario).toFixed(2)}</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>INSS</td>
-            <td></td>
-            <td>${(object.desconto.inss).toFixed(2)}</td>
-        </tr>
-        <tr>
-            <td>IRRF</td>
-            <td></td>
-            <td>${(object.desconto.irrf).toFixed(2)}</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><strong>total dos proventos</strong</td>
-            <td><strong>total dos descontos</storng></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>${(object.provento.total).toFixed(2)}</td>
-            <td>${(object.desconto.total).toFixed(2)}</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>Salário Líquido</td>
-            <td>${(object.salarioLiquido).toFixed(2)}</td>
-        </tr>
-    </table>`
+function obterTotal(provento, desconto) {
+    return provento.total - desconto.total
 }
